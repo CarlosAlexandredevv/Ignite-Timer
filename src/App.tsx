c164d38047df0aa002c6eca4553957f21c5cc85f
Carlos Alexandre <alexfilho062005@gmail.com>
2024-11-06 13:41:33 -0300

    feat: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
 
 function App() {
-  return <ThemeProvider theme={defaultTheme}></ThemeProvider>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
 
 export default App;
