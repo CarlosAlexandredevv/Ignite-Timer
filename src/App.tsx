45df00b7052c7e77d208a1c748f6a338f3b64c62
Carlos Alexandre <alexfilho062005@gmail.com>
2024-11-06 12:10:46 -0300

    feat: define application themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default';
+
 function App() {
-  return <div></div>;
+  return <ThemeProvider theme={defaultTheme}></ThemeProvider>;
 }
 
 export default App;
