b3e28e6225ee3a571cf6fb177c8f0acb329a4d03
Carlos Alexandre <alexfilho062005@gmail.com>
2024-11-29 19:04:17 -0300

    project finished!

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,13 +1,16 @@
 import { useContext } from 'react';
 import { formatDistanceToNow } from 'date-fns';
-import { ptBR } from 'date-fns/locale';
+import ptBR from 'date-fns/locale/pt-BR';
 import { CyclesContext } from '../../contexts/CyclesContext';
 import { HistoryContainer, HistoryList, Status } from './styles';
+
 export function History() {
   const { cycles } = useContext(CyclesContext);
+
   return (
     <HistoryContainer>
       <h1>Meu histórico</h1>
+
       <HistoryList>
         <table>
           <thead>
@@ -25,7 +28,7 @@ export function History() {
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
                   <td>
-                    {formatDistanceToNow(cycle.startDate, {
+                    {formatDistanceToNow(new Date(cycle.startDate), {
                       addSuffix: true,
                       locale: ptBR,
                     })}
@@ -34,9 +37,11 @@ export function History() {
                     {cycle.finishedDate && (
                       <Status statusColor="green">Concluído</Status>
                     )}
+
                     {cycle.interruptedDate && (
                       <Status statusColor="red">Interrompido</Status>
                     )}
+
                     {!cycle.finishedDate && !cycle.interruptedDate && (
                       <Status statusColor="yellow">Em andamento</Status>
                     )}
